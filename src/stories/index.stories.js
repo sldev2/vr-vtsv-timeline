80fa3f48332c2729d0dbde2c8c666eee5a210bc2
Spero Larres <slarres3@gmail.com>
2021-08-03 07:49:45 -0400

    added storybook

---
+++ src/stories/index.stories.js
@@ -0,0 +1,19 @@
+import { action } from '@storybook/addon-actions'
+
+import MyButton from '../components/MyButton.vue'
+
+export default {
+  title: 'Button',
+}
+
+export const withText = () => ({
+  components: { MyButton },
+  template: '<my-button @click="action">Hello Button</my-button>',
+  methods: { action: action('clicked') }
+})
+
+
+export const withSomeEmoji = () => ({
+  components: { MyButton },
+  template: '<my-button>😀 😎 👍 💯</my-button>'
+})
